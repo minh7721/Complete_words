c0e382622049015ad9f0dcf0ea97cd74ff6c9f6e
Nguyễn Văn Hiệp <nguyenhiepvan.bka@gmail.com>
2021-08-31 16:48:54 +0700

    remove pdf

--- test.php
+++ test.php
@@ -12,9 +12,6 @@ use Nguyenhiep\CompleteWords\Complete_words;
 
 $files = array_diff(scandir(__DIR__ . "/test"), ['.', '..']);
 foreach ($files as $file) {
-    if (!str_contains($file, "pdf")) {
-        continue;
-    }
     $info = pathinfo($file);
     if (file_exists($dictionary = __DIR__ . "/test/{$info['filename']}_text.txt")
         && file_exists($cases_file = __DIR__ . "/test/{$info['filename']}.txt")
